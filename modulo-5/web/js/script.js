b7ae312110ef0c0bb05112ec1688a7ba6e9ac3ee
Mira757 <joao.veloz757@gmail.com>
2021-08-06 18:17:46 -0300

    Faltando apenas a ultima aula

--- modulo-5/web/js/script.js
+++ modulo-5/web/js/script.js
@@ -16,3 +16,12 @@ new SimpleSlide({
 if (window.SimpleAnime) {
   new SimpleAnime
 } 
+
+if (window.SimpleAnime) {
+new SimpleForm ({
+  form: ".formphp",
+  button: "#enviar",
+  erro: "<div id='form-erro><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email joaomira@byronsolutions.com<p></div>",
+  sucesso: "<div id='form-sucesso><p>Formulário enviado com sucesso</h2><p>Em breve eu entro em cotato com você.<p></div>",
+});
+}
